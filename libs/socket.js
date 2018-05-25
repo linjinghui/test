3d975b01503644403e7b9ab95adbce08291a9ef4
linjinghui <852530363@qq.com>
2018-05-25 13:48:59 +0800

    fixed

---
+++ libs/socket.js
@@ -0,0 +1,47 @@
+
+export default function (opt) {
+  // "ws://"+location.host+"/cfws/conference/";
+  // "wss://47.93.80.37:8050/module-conference/conference/";
+  let option = Object.assign({
+    url: '',
+    onOpen: function () {},
+    onMessage: function () {},
+    onError: function () {},
+    onClose: function () {},
+    sendMessage: function () {}
+  }, opt);
+  let ws = connectWs(url);
+
+  if (!url) {
+    return '';
+  }
+
+  // 创建socket连接
+  function connectWs (url) {
+    return new WebSocket(url);
+  }
+
+  ws.onopen = function () {
+    // socket 连接成功
+    option.onOpen();
+  }
+
+  ws.onmessage = function (evt) {
+    // 服务端推送
+    // console.info("=================");
+    // console.info("1、此次推送内容：" + evt.data);
+    option.onMessage(evt.data);
+  }
+
+  ws.onerror = function (evt) {
+    // socket 错误
+    option.onError(evt);
+  }
+
+  ws.onclose = function (evt) {
+    // socket连接关闭
+    // console.info('socket连接关闭，准备重新连接，当前连接次数：' + _this.socket.connectNum)
+    option.onClose(evt);
+  }
+  return ws;
+}
--- libs/tool.js
+++ libs/tool.js
@@ -589,3 +589,49 @@ export function getFileSuffix (fileName) {
   }
   return result;
 }
+
+/**
+*生成唯一ID
+*/
+export function guid () {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
+}
+
+/**
+*解析类似地址栏Url格式的字符串
+*/
+export function parseUrlStr (str) {
+  str = str.split('://');
+  str = str[str.length - 1];
+  str = str.split('?');
+  var hostname = str[0] || '';
+  var search = str[1] || '';
+  var query = '';
+  if (search) {
+    query = search.replace(/=/g, '":"').replace(/&/g, '","');
+    query = '{"' + query + '"}';
+    try { query = JSON.parse(query); } catch (e) { query = ''; }
+  }
+  return {
+    hostname: hostname,
+    search: search,
+    query: query
+  };
+}
+
+/**
+*计算dom元素是否在可见范围之内
+*/
+export function isVisible (dom, parantDom) {
+  let result = '';
+
+  if (dom && parantDom) {
+    result = !(parantDom.scrollTop >= (dom.offsetTop + dom.offsetHeight) || (parantDom.scrollTop + parantDom.offsetHeight) <= (dom.offsetTop + dom.offsetHeight));
+  }
+  // console.log('(parantDom.scrollTop >= (dom.offsetTop + dom.offsetHeight) || (parantDom.scrollTop + parantDom.offsetHeight) <= (dom.offsetTop + dom.offsetHeight))');
+  // console.log('(' + parantDom.scrollTop + '>=(' + dom.offsetTop + '+' + dom.offsetHeight + ') || (' + parantDom.scrollTop + '+' + parantDom.offsetHeight + ') <= (' + dom.offsetTop + '+' + dom.offsetHeight + '))');
+  return result;
+}
